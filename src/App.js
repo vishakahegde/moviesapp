f4b4433f45d9c6d7d087be30ffe71a40bda3f92e
vishakahegde <hegde.vishaka@gmail.com>
2020-05-01 14:51:52 +0200

    Styled the navlink in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavLink
+        exact={true}
+        to="/discover"
+        activeStyle={{
+          fontWeight: "bolder",
+          color: "green",
+        }}
+      />
+      <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
